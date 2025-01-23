0cd23e128a6d8e414f8fc62c92bf46c8c4774441
Francisco Ramirez <py.francisco.ramirez@gmail.com>
2025-01-23 14:16:55 -0600

    chore(app): Black code linting

--- accounts/admin.py
+++ accounts/admin.py
@@ -5,6 +5,7 @@ from django.contrib.auth.admin import UserAdmin
 from .forms import CustomUserCreationForm, CustomUserChangeForm
 from .models import CustomUser
 
+
 # Register your models here.
 class CustomUserAdmin(UserAdmin):
     add_form = CustomUserCreationForm
@@ -16,5 +17,6 @@ class CustomUserAdmin(UserAdmin):
         "name",
         "is_staff",
     ]
+
 
 admin.site.register(CustomUser, CustomUserAdmin)
