600197fbd6d496699aa170c5b23083ee3d0081ed
Francisco Ramirez <py.francisco.ramirez@gmail.com>
2025-02-03 17:32:13 -0600

    feat(project): add static schema

--- django_project/settings.py
+++ django_project/settings.py
@@ -47,6 +47,7 @@ INSTALLED_APPS = [
     "allauth.socialaccount",
     "dj_rest_auth",
     "dj_rest_auth.registration",
+    "drf_spectacular",
     # Local
     "accounts.apps.AccountsConfig",
     "posts.apps.PostsConfig",
@@ -153,6 +154,7 @@ REST_FRAMEWORK = {
         "rest_framework.authentication.SessionAuthentication",
         "rest_framework.authentication.TokenAuthentication",
     ],
+    "DEFAULT_SCHEMA_CLASS": "drf_spectacular.openapi.AutoSchema",
 }
 
 ## CORS headers
@@ -162,5 +164,11 @@ CORS_ORIGIN_WHITELIST = (
 )
 
 ## CSRF trusted origins
-CSRF_TRUSTED_ORIGINS = ["http://localhost:300"
-                        "0"]
+CSRF_TRUSTED_ORIGINS = ["http://localhost:300" "0"]
+
+## Django Spectacular
+SPECTACULAR_SETTINGS = {
+    "TITLE": "Blog API Project",
+    "DESCRIPTION": "A sample blog to practice DRF",
+    "VERSION": "1.0.0",
+}
